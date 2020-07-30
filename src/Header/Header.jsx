534e1c42944b807505e7acc49d099ba7c2617ca6
Ivan <ivansolobear@gmail.com>
2020-07-30 14:21:13 +0300

    fix: header now recieves props

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 import "./Header.css";
 
-const Header = () => {
-
-    
-
-
+const Header = ({ urlInput, setUrlInput }) => {
   return (
     <div className="header" data-testid="header">
       <div>URL:</div>
