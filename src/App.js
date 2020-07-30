62979262e96a68a80ffc8b348de4d3409fab5779
Ivan <ivansolobear@gmail.com>
2020-07-30 20:34:33 +0300

    cleared some boilerplate

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import Header from './Header/Header';
 import JsonItem from './components/JsonItem/JsonItem'
 
 function App() {
 
-  const [urlInput, setUrlInput] = useState('http://www.7timer.info/bin/api.pl?lon=113.17&lat=23.09&product=astro&output=json');
+  const [urlInput, setUrlInput] = useState('');
   const [jsonResponse, setJsonResponse] = useState({});
 
   const fetchJson = inputValue =>
