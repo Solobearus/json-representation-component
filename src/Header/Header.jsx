83ff50d0403c997672558dca82a935e0f1b4f185
Ivan <ivansolobear@gmail.com>
2020-07-30 14:20:33 +0300

    added Header to app

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
-import './Header.css'
-
+import React from "react";
+import "./Header.css";
 
 const Header = () => {
-    return (
-        < div className="header" data-testid="header">
 
-        </div >
-    )
-}
+    
 
-export default Header
+
+  return (
+    <div className="header" data-testid="header">
+      <div>URL:</div>
+      <input
+        type="text"
+        value={urlInput}
+        onChange={(e) => setUrlInput(e.target.value)}
+      />
+      <button onClick={() => {}}>Show JSON</button>
+    </div>
+  );
+};
+
+export default Header;
