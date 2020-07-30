0fa7dfdaa2586315413928f574b1b038bde18c31
Ivan <ivansolobear@gmail.com>
2020-07-30 14:22:29 +0300

    add: added jsonItem

---
+++ src/JsonItem/JsonItem.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './JsonItem.css'
+
+
+const JsonItem = () => {
+    return (
+        < div className="jsonItem" data-testid="jsonItem">
+
+        </div >
+    )
+}
+
+export default JsonItem
