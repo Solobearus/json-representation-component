5192125c6aecf2d51dbb6c468d37c963571b2718
Ivan <ivansolobear@gmail.com>
2020-07-31 00:28:30 +0300

    added : empty objects and arrays now cannot be expanded

--- src/components/RepresentationOfKey/RepresentationOfKey.jsx
+++ src/components/RepresentationOfKey/RepresentationOfKey.jsx
@@ -4,7 +4,7 @@ import "./RepresentationOfKey.css";
 const formatKey = (keyOfNode) => (keyOfNode || keyOfNode === 0 ? `${keyOfNode} : ` : "");
 
 export const RepresentationOfKey = ({ typeOfJsonValue, open, setOpen, keyOfNode, jsonValue }) =>
-  typeOfJsonValue === "object" && jsonValue !== null ? (
+  typeOfJsonValue === "object" && jsonValue !== null && Object.keys(jsonValue).length > 0 ? (
     <span onClick={() => setOpen(!open)} className={"key"}>
       {`${open ? "-" : "+"} ${formatKey(keyOfNode)}`}
     </span>
