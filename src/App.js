06d3910430f8e774e3fd8005c42b05dd88a6cb18
Ivan <ivansolobear@gmail.com>
2020-07-30 21:57:29 +0300

    added error handeling

--- src/App.js
+++ src/App.js
@@ -5,22 +5,29 @@ import JsonItem from './components/JsonItem/JsonItem'
 
 function App() {
 
-  // This is a random api I left for your ease of use 
-  const [urlInput, setUrlInput] = useState('https://archive.org/advancedsearch.php?q=subject:google+sheets&output=json')
+  const [urlInput, setUrlInput] = useState('')
   const [jsonResponse, setJsonResponse] = useState({});
   const [loading, setLoading] = useState(false);
-
+  const [error, setError] = useState('')
+  
   const fetchJson = async (inputValue) => {
     setLoading(true);
+    setError('');
+    
     await fetch(inputValue)
       .then(res => res.json())
-      .then(res => setJsonResponse(res))
-    setLoading(false)
+      .then(res => {
+        console.log(res)
+        setJsonResponse(res)})
+      .catch(error => {setError("ERROR : " + error + ' --- please check that the url is valid')});
+
+    setLoading(false);  
   }
 
   return (
     <div className="App" data-testid="App">
       <Header urlInput={urlInput} setUrlInput={setUrlInput} fetchJson={fetchJson} loading={loading} />
+      <div className="error">{error}</div>
       <div className="JsonHolder">
         {jsonResponse && Object.keys(jsonResponse).length > 0 && <JsonItem jsonValue={jsonResponse} />}
       </div>
