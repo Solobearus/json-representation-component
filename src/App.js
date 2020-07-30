62347ba8389e28de08d87e5ae8521e9d7ab0f381
Ivan <ivansolobear@gmail.com>
2020-07-30 20:55:14 +0300

    made some styling fixes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React, { useState } from 'react';
-import Header from './Header/Header';
+import Header from './components/Header/Header';
+import './App.css';
 import JsonItem from './components/JsonItem/JsonItem'
 
 function App() {
 
-  const [urlInput, setUrlInput] = useState('');
+  const [urlInput, setUrlInput] = useState('https://archive.org/advancedsearch.php?q=subject:google+sheets&output=json')
   const [jsonResponse, setJsonResponse] = useState({});
 
-  const fetchJson = inputValue =>
+  const fetchJson = (inputValue) =>
     fetch(inputValue)
       .then(res => res.json())
       .then(res => setJsonResponse(res))
 
+
   return (
-    <div className="App">
+    <div className="App" data-testid="App">
       <Header urlInput={urlInput} setUrlInput={setUrlInput} fetchJson={fetchJson} />
-      <div className="JsonWrapper">
+      <div className="JsonHolder">
         {jsonResponse && Object.keys(jsonResponse).length > 0 && <JsonItem jsonValue={jsonResponse} />}
       </div>
     </div>
-  );
+  )
 }
 
 export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,12 +5,14 @@ const Header = ({ urlInput, setUrlInput, fetchJson }) => {
   return (
     <div className="header" data-testid="header">
       <div>URL:</div>
-      <input
-        type="text"
-        value={urlInput}
-        onChange={(e) => setUrlInput(e.target.value)}
-      />
-      <button onClick={() => {fetchJson(urlInput)}}>Fetch JSON</button>
+      <input className="header__input" type="text" value={urlInput} onChange={(e) => setUrlInput(e.target.value)} />
+      <button
+        onClick={() => {
+          fetchJson(urlInput);
+        }}
+      >
+        Fetch JSON
+      </button>
     </div>
   );
 };
